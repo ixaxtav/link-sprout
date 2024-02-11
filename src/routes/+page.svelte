44b5a64e3e4d30526927963dd22a6b845972b9bc
Ixax Tavarez <ixaxtav@gmail.com>
2024-02-11 03:00:50 -0500

    created multi step form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-red-800">Welcome to SvelteKit</h1>
+<button class="btn btn-error">Button</button>
+
