3c3d27b3931e830dbdeeac9aa45ec4b1dc473d3d
Ixax Tavarez <ixaxtav@gmail.com>
2024-02-11 03:28:49 -0500

    added google sign in

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,18 @@
 <script lang="ts">
+    import { auth } from '$lib/firebase';
+    import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
+    async function signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        try {
+            const result = await signInWithPopup(auth, provider);
+            console.log(result);
+        } catch (error) {
+            console.error(error);
+        }
+    }
 </script>
 
 <h2>Login</h2>
+
+<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
