d79209e07de78b97a950f30068bfd0b5deb47024
Ixax Tavarez <ixaxtav@gmail.com>
2024-02-11 18:50:02 -0500

    create userStore to handle auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB4S-gtPVAU4Xam_VAJXn7k16dFVjsn-PU",
@@ -17,3 +18,31 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+function userStore() {
+  let unsubscribe: () => void;
+
+  if (!auth || !globalThis.window) {
+    console.warn("auth or window not available");
+
+    const { subscribe } = writable(null);
+
+    return {
+      subscribe,
+    };
+  }
+
+  const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+    unsubscribe = onAuthStateChanged(auth, (user) => {
+      set(user);
+    });
+
+    return () => unsubscribe();
+  });
+
+  return {
+    subscribe,
+  };
+}
+
+export const user = userStore();
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { auth } from '$lib/firebase';
+    import { auth, user } from '$lib/firebase';
     import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
     async function signInWithGoogle() {
@@ -10,9 +10,16 @@
         } catch (error) {
             console.error(error);
         }
-    }
+    } 
 </script>
 
 <h2>Login</h2>
 
-<button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+{#if $user}
+    <h2 class="card-title">Welcome, {$user.displayName}</h2>
+    <p>You are logged in</p>
+    <button class="btn btn-danger" on:click={signOut}>Sign out</button>
+{:else}
+    <button class="btn btn-primary" on:click={signInWithGoogle}>Sign in with Google</button>
+{/if} 
+
