3355786900995c15fbcaa79334df2057aaa7efe6
Ixax Tavarez <ixaxtav@gmail.com>
2024-02-11 03:13:12 -0500

    created AnimatedRoute component

--- src/routes/login/+layout.svelte
+++ src/routes/login/+layout.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import {page} from '$app/stores';
+    import AnimatedRoute from '$lib/components/AnimatedRoute.svelte';
 </script>
 
 <nav class="nav flex justify-center my-6">
@@ -9,9 +10,10 @@
         <a href="/login/photo" class="step" class:step-primary={$page.route.id?.includes("photo")}>Upload Photo</a>
       </ul>
 </nav>
-
-<main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
-    <div class="card-body items-center text-center">
-        <slot/>
-    </div>
-</main>
+<AnimatedRoute>
+    <main class="card w-4/6 bg-neutral text-neutral-content mx-auto">
+        <div class="card-body items-center text-center">
+            <slot/>
+        </div>
+    </main>
+</AnimatedRoute>
