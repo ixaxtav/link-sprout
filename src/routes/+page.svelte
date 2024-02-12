458a1fe55350b822ea371fad897cde7ab36d8121
Ixax Tavarez <ixaxtav@gmail.com>
2024-02-12 15:14:22 -0500

    added homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,12 @@
-<h1 class="text-red-800">Welcome to SvelteKit</h1>
-<button class="btn btn-error">Button</button>
-
+<main class="flex w-full min-h-screen">
+    <div class="hero bg-slate-100">
+        <div class="hero-content text-center">
+            <div class="w-full">
+                <h1 class="text-7xl font-bold text-black">🌱 Link Sprout</h1>
+                <p class="py-6 text-2xl text-black">Unify your professional portfolio with a single link</p>
+                <a href="/login" class="btn btn-primary">Get Started</a>
+                <a href="/ixax-tavarez" class="btn btn-secondary">Example Profile</a>
+            </div>
+        </div>
+    </div>
+</main>
